45b2154a7c936fd3ec6c0e901b5c5753eed2d460
SofianeLasri <alasri250@gmail.com>
2021-12-22 11:55:20 +0100

    color: white

--- pages/client/vitrine.php
+++ pages/client/vitrine.php
@@ -8,9 +8,9 @@
     <title>Marmiuton</title>
 </head>
 <body>
-    <div class="container-fluid mainColor-bg">
+    <div id="cookieConsent" class="container-fluid mainColor-bg">
         <div class="container">
-            <span class="text-white">Nous utilisons des cookies pour vous garantir la meilleure expérience sur notre site. <i class="fas fa-cookie"></i></span>
+            <span>Nous utilisons des cookies pour vous garantir la meilleure expérience sur notre site. <i class="fas fa-cookie"></i></span>
         </div>
     </div>
 </body>
