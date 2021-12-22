583a069966bc3c1edf11c0efd446710e21e38b57
SofianeLasri <alasri250@gmail.com>
2021-12-22 10:12:18 +0100

    utilisation du fichier de config pour la connexion

--- core/conf/ConfigurationGenerale.php
+++ core/conf/ConfigurationGenerale.php
@@ -1,6 +1,9 @@
 <?php
-// Ça aurait été sympa de tout regrouper ici, mais le fait que la classe Connnexion ne puisse inclure de fichiers et que cela reviendrait à s'écarter du cours de faire autre chose....
-// C'est pas très propre mais bon, ça fonctionne
+// attributs de la classe Connexion paramètres de connexion à la base
+$bddHost = 'localhost';
+$bddName = 'marmiuton';
+$bddUsername = 'marmiuton';
+$bddUserPassword = 's6rqTiA0hNKmcgy7';
 
 // Cette variable défini le mode de reconnaissance des url
 // Sur le serveur de l'IUT, on ne peut pas utiliser les alias de l'url pour détecter les pages, vu que nous ne sommes pas sur la racine du domaine
