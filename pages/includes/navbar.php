f39d2170071cda919d94baacac5ca3da75937295
SofianeLasri <alasri250@gmail.com>
2021-12-29 16:02:48 +0100

    search

--- pages/includes/navbar.php
+++ pages/includes/navbar.php
@@ -32,9 +32,8 @@
                     <a class="nav-link" href="#">Communauté</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <form id="search" class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Je cherche une <strong>recette</strong>, un <strong>ingrédient</strong>...">
             </form>
         </div>
     </div>
