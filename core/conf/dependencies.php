0e0b7bffa213855526d5dc29e5a9a68345938ce1
SofianeLasri <alasri250@gmail.com>
2021-12-31 11:36:28 +0100

    Favicon

--- core/conf/dependencies.php
+++ core/conf/dependencies.php
@@ -2,6 +2,11 @@
 // Ce fichier contient les dépendances HTML/CSS/JS des pages du site
 // Il faut donc l'inclure dans le "head" de chaque page
 ?>
+<!-- Icône du site -->
+<link rel="icon" type="image/svg+xml" href="/data/images/logo/favicon.svg">
+<link rel="alternate icon" href="/data/images/logo/favicon.ico">
+<link rel="mask-icon" href="/data/images/logo/favicon.svg" color="#ed8930">
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
