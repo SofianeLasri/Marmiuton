2e9051f8b8f72077a9e10a88bd5f493a233b84f8
SofianeLasri <alasri250@gmail.com>
2022-01-02 14:49:17 +0100

    footer stick to bottom

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -33,3 +33,15 @@
         </div>
     </div>
 </footer>
+<script type="text/javascript">
+	// Permet de correctement placer la barre de naviguation
+	reportWindowSize();
+	function reportWindowSize() {
+		if (document.body.clientHeight<window.innerHeight) {
+			$("footer").css("position", "fixed");
+		} else {
+			$("footer").css("position", "relative");
+		}
+	}
+	window.onresize = reportWindowSize;
+</script>
