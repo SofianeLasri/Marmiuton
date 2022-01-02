67e6a4f02e105df77de3d996a55bf141388cd2b2
SofianeLasri <alasri250@gmail.com>
2022-01-02 12:15:24 +0100

    footer

--- pages/client/vitrine.php
+++ pages/client/vitrine.php
@@ -61,22 +61,7 @@
 
         <div class="row">
             <!-- Carte recette -->
-            <div class="col-md-6 mt-5">
-                <div class="carte-recette">
-                    <a href="#">
-                        <div class="carte-recette-img" style="background-image: url('/data/images/plat-delicieux1.jpg');"></div>
-                    </a>
-                    <div class="carte-recette-infos">
-                        <h4><strong>Recette 1</strong></h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quia.</p>
-                        <i><a href="#" class="text-orange">Marmiuton</a> <span class="text-muted"><i class="far fa-stopwatch"></i> 75 minutes</span></i>
-                    </div>
-                </div>
-            </div>
-            <!-- Fin -->
-
-            <!-- Carte recette -->
-            <div class="col-md-6 mt-5">
+            <div class="col-md-6">
                 <div class="carte-recette">
                     <a href="#">
                         <div class="carte-recette-img" style="background-image: url('/data/images/plat-delicieux1.jpg');"></div>
@@ -106,7 +91,22 @@
             <!-- Fin -->
 
             <!-- Carte recette -->
-            <div class="col-md-6 mt-5">
+            <div class="col-md-6">
+                <div class="carte-recette">
+                    <a href="#">
+                        <div class="carte-recette-img" style="background-image: url('/data/images/plat-delicieux1.jpg');"></div>
+                    </a>
+                    <div class="carte-recette-infos">
+                        <h4><strong>Recette 1</strong></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quia.</p>
+                        <i><a href="#" class="text-orange">Marmiuton</a> <span class="text-muted"><i class="far fa-stopwatch"></i> 75 minutes</span></i>
+                    </div>
+                </div>
+            </div>
+            <!-- Fin -->
+
+            <!-- Carte recette -->
+            <div class="col-md-6">
                 <div class="carte-recette">
                     <a href="#">
                         <div class="carte-recette-img" style="background-image: url('/data/images/plat-delicieux1.jpg');"></div>
@@ -121,6 +121,42 @@
             <!-- Fin -->
         </div>
     </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="block-titres">
+                        <h3><strong>À propos de nous</strong></h3>
+                    </div>
+                    <div>
+                        <p>Le site Marmiuton a été créé par <a href="https://sl-projects.com" target="_blank">Sofiane Lasri</a><a href="mailto:sofiane.lasri-trienpont@universite-paris-saclay.fr"><i class="fas fa-envelope"></i></a>  et <a href="mailto:yann.jeanmaire-dit-cartier@universite-paris-saclay.fr">Yann Jeanmire-dit-Cartier</a>, dans le cadre d'un projet tutoré organisé par <a href="https://www.iut-orsay.universite-paris-saclay.fr/" target="_blank">L'IUT d'Orsay</a>.</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="block-titres">
+                        <h3><strong>Liens utiles</strong></h3>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="/login">Connexion espace membre</a></li>
+                            <li><a href="/recettes">Recettes</a></li>
+                            <li><a href="/login">Connexion espace membre</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="block-titres">
+                        <h3><strong>Dernières recettes</strong></h3>
+                    </div>
+                    <div>
+                        <p>Plus-tard, trop la flemme là.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <script src="/pages/assets/vendors/flickity/js/flickity.pkgd.min.js"></script>
     <script type="text/javascript">
