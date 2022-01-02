76c8fdbf76ff2cf4310697ffb1546ceef5c4fc0c
suprasanity <yann.jeanmaire@gmail.com>
2022-01-02 16:04:21 +0100

    yann bourdel

---
+++ core/controller/controllerLogin.php
@@ -0,0 +1,18 @@
+<?php 
+require_once("./model/login.php");
+
+class controllerLogin {
+
+
+	public static function create() {
+		require("view/loginTemp.php");
+	}
+
+	public static function created() {
+		$nom = $_POST["nom"];
+		$prenom = $_POST["prenom"];
+		$email = $_POST["email"];
+        $mdp =$_POST["mdp"];
+		login::addUser($nom,$prenom,$email,$mdp);
+	}
+}
