7626e799bed3bd5acb6d77fc4acb4abba97bd77b
SofianeLasri <alasri250@gmail.com>
2022-01-02 16:42:53 +0100

    ahhh

--- index.php
+++ index.php
@@ -4,16 +4,9 @@ require_once "core/conf/ConfigurationGenerale.php"; // Ce fichier contient diver
 require_once "core/conf/Connexion.php"; // Ce fichier se charge de la connexion à la base de donnée
 require_once "core/controller/variables.php"; // Ce fichier se charge de récupérer les variables globales
 require_once "core/controller/functions.php"; // Et celui-ci des différentes fonctions
-<<<<<<< HEAD
-=======
-require_once "core/controller/controllerLogin.php";
->>>>>>> dfee41b035669de2956860609a5ba654cc14a3ca
 
-// On initialise la connexion à la base de donnée
+// On initialise la connexion à la base de données
 Connexion::connect();
-<<<<<<< HEAD
 
-=======
->>>>>>> 5917908ca4a09db272693a1faf0a13c071a68e34
 // Et on appelle la page demandée
 loadPage();
