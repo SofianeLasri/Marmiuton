35b6d9c4560b266a0ea8de5ef309794e7c244fe0
SofianeLasri <alasri250@gmail.com>
2022-01-03 11:39:35 +0100

    fixed

--- pages/client/loginTemp.php
+++ pages/client/loginTemp.php
@@ -232,14 +232,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])
 		    // Fetch all the forms we want to apply custom Bootstrap validation styles to
 		    var forms = document.getElementsByClassName('needs-validation');
 		    // Loop over them and prevent submission
-			document.getElementById("registerBtn").addEventListener("click", function() {
-				if(form.checkValidity() === false){
+			var validation = Array.prototype.filter.call(forms, function(form) {
+				document.getElementById("registerBtn").addEventListener("click", function() {
+					if(form.checkValidity() === false){
 
-				}else{
-					sendFormData();
-				}
-				form.classList.add('was-validated');
-			});
+					}else{
+						sendFormData();
+					}
+					form.classList.add('was-validated');
+				});
+		    });
+			
 
 			/*
 		    var validation = Array.prototype.filter.call(forms, function(form) {
