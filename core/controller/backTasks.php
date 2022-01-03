35aa159aceeb91575c42d9525eca18760027a362
SofianeLasri <alasri250@gmail.com>
2022-01-03 10:50:49 +0100

    test: echo true;

--- core/controller/backTasks.php
+++ core/controller/backTasks.php
@@ -5,7 +5,7 @@
 // Faut-il alors intégrer son code au fichier fonction? Bah c'est compliqué car il compare pas mal de choses avant d'executer ces fonctions...
 // ----- / Il sert donc de passerelle entre fonctions.php et les pages.
 
-echo "test";
+echo true;
 if(isset($_GET["checkUsernameEmail"]) && !empty($_GET["checkUsernameEmail"])){
     // Cette fonction va vérifier si un username ou un email existe déjà dans la bdd
     echo checkUsernameEmail($_GET["checkUsernameEmail"]);
