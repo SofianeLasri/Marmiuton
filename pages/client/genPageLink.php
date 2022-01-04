7943791acbbf01a7dc6d1747cf496b314a904f12
SofianeLasri <alasri250@gmail.com>
2022-01-04 18:03:17 +0100

    comment

--- pages/client/genPageLink.php
+++ pages/client/genPageLink.php
@@ -4,9 +4,18 @@
     echo "<br>/admin/test?info=value - ".genPageLink("/admin/test?info=value");
     // Là ça marche
     echo "<br>/admin/test/?info=value - ".genPageLink("/admin/test/?info=value");
+    
+    // Pareil
     echo "<br>/recettes?search=valeur - ".genPageLink("/recettes?search=valeur");
+    // Là c'est bon
     echo "<br>/recettes/?search=valeur - ".genPageLink("/recettes/?search=valeur");
     echo "<br>/login - ".genPageLink("/login");
+
+    // Cela implique que l'on ne peut pas avoir autre que chose que soit du vide, soit des paramètres dans l'alias qui défini la page
+    // ex:
+    // /recettes/patate/?key=value     Ne marchera pas car /patate prend l'emplacement des paramètres
+
+    // En fait cette fonction n'est utile que pour faire fonctionner le site avec le mode "paramètres". Elle ne s'active pas en mode alias.
 ?>
 <h3>Cas concret en HTML:</h3>
 
