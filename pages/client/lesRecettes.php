d337a30c7989136c1df130c50990383482fa85c4
suprasanity <66360950+suprasanity@users.noreply.github.com>
2022-01-04 17:29:44 +0100

    Update lesRecettes.php

--- pages/client/lesRecettes.php
+++ pages/client/lesRecettes.php
@@ -17,7 +17,15 @@
     <!-- Inclusion dynamique de la navbar -->
     <?=getNavbar()?>
 
-
+    <div class="container">
+  <div class="row">
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="w-100"></div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</div>
 
 
 
