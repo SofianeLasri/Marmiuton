389a3bb0237111b040cb10d0b03d4877ad12b14b
SofianeLasri <alasri250@gmail.com>
2022-01-06 15:03:01 +0100

    :)

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -14,14 +14,14 @@
 			<div class="userLogo" style="background-image: url('<?=$_SESSION['userProfilePic']?>');"></div>
 			<div class="ml-5">
 				<h3>Bienvenue <?=$_SESSION["userName"]?>!</h3>
-				<strong>Je tiens à préciser que cette partie du site a été réalisée de manière totalement déloyale. :)</strong></p>
+				<strong>Je tiens à préciser que cette partie du site a été réalisée à "l'arrache" de manière totalement déloyale. :)</strong></p>
 			</div>
 		</div>
 	</div>
 	<div class="page-content notTop" leftSidebar="240" rightSidebar="0">
 		<h3>Tableau de bord</h3>
 		<p>Bienvenu sur le paneau d’administration. Voici un bref résumé de l'activité de cette semaine.</p>
-		<p>Cette page d'acceuil est décidément condamnée à être vide. On a clairement pas le temp de développer. 
+		<p>Cette page d'acceuil est décidément condamnée à rester vide. On a clairement pas le temps de la développer. 
 		<br><code><pre><?php print_r($_SESSION); ?></pre></code></p>
 
 		
