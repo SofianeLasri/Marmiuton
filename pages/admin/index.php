051aa3aa806e06d522e4866287bd607ae6cee8c5
SofianeLasri <alasri250@gmail.com>
2022-01-06 15:01:49 +0100

    index

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -14,20 +14,15 @@
 			<div class="userLogo" style="background-image: url('<?=$_SESSION['userProfilePic']?>');"></div>
 			<div class="ml-5">
 				<h3>Bienvenue <?=$_SESSION["userName"]?>!</h3>
-				<br><strong>Vous êtes sur une version de développement de VBcms.</strong></p>
+				<strong>Je tiens à préciser que cette partie du site a été réalisée de manière totalement déloyale. :)</strong></p>
 			</div>
 		</div>
 	</div>
 	<div class="page-content notTop" leftSidebar="240" rightSidebar="0">
 		<h3>Tableau de bord</h3>
 		<p>Bienvenu sur le paneau d’administration. Voici un bref résumé de l'activité de cette semaine.</p>
-		
-		<div class="mt-3">
-			<h3>Merci de participer aux tests!</h3>
-			<p>Salut et merci d'avoir accepté de participer aux tests de pré-sortie. Tu es là sur une version très primaire de ce que sera VBcms 2.0 à la fin, et comme tu l'as peut-être déjà remarqué, il n'y a pas grand chose de terminé.</p><br>
-			<?php print_r($_SESSION); ?>
-			
-		</div>
+		<p>Cette page d'acceuil est décidément condamnée à être vide. On a clairement pas le temp de développer. 
+		<br><code><pre><?php print_r($_SESSION); ?></pre></code></p>
 
 		
 		
