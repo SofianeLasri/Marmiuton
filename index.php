0f800d8d4fe2b6b9816ed01cac1d88cf0b3380dd
SofianeLasri <alasri250@gmail.com>
2022-01-07 10:45:02 +0100

    fixed?

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
-// Ici on commence par intégrer les différents fichiers qui nous serviront à faire fonctionner le site
-require_once "core/conf/ConfigurationGenerale.php"; // Ce fichier contient divers paramètres
-require_once "core/conf/Connexion.php"; // Ce fichier se charge de la connexion à la base de donnée
-
-// On initialise la connexion à la base de données
-Connexion::connect();
-// On récupère la durée de la session en secondes
-$cookieDuration = Connexion::pdo()->query("SELECT value FROM m_siteSetting WHERE name='cookieDuration'")->fetchColumn();
 // On change la durée de la session
-ini_set('session.gc_maxlifetime', $cookieDuration);
+ini_set('session.gc_maxlifetime', 1209600); // 14 jours
 // On demande au client de garder l'id de la sesssion aussi longtemps que sa durée
-session_set_cookie_params($cookieDuration);
+session_set_cookie_params(1209600);
 // Et on démarre la session
 session_start();
 
+// Ici on commence par intégrer les différents fichiers qui nous serviront à faire fonctionner le site
+require_once "core/conf/ConfigurationGenerale.php"; // Ce fichier contient divers paramètres
+require_once "core/conf/Connexion.php"; // Ce fichier se charge de la connexion à la base de donnée
 require_once "core/controller/variables.php"; // Ce fichier se charge de récupérer les variables globales
 require_once "core/controller/functions.php"; // Et celui-ci des différentes fonctions
+
+// On initialise la connexion à la base de données
+Connexion::connect();
 
 // Enfin on appelle la page demandée
 loadPage();
