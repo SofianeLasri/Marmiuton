83690df091d4b4d4a1bffed30562fc3c090483b7
SofianeLasri <alasri250@gmail.com>
2022-01-07 11:43:58 +0100

    fix2

--- pages/admin/settings.php
+++ pages/admin/settings.php
@@ -15,7 +15,7 @@
 
         <form id="settingsForm" class="width-50em">
             <?php
-                $settings = Connexion::pdo()->query("SELECT * FROM settings")->fetchAll(PDO::FETCH_ASSOC);
+                $settings = Connexion::pdo()->query("SELECT * FROM m_siteSetting")->fetchAll(PDO::FETCH_ASSOC);
                 foreach($settings as $setting){
                     echo ('<div class="form-group">
                         <label>'.$setting["name"].'</label>
