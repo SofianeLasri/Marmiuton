29580a54204bc0851624bfb79f09843c0372fb54
SofianeLasri <alasri250@gmail.com>
2022-01-09 11:53:14 +0100

    editor for ecrireRecette

--- pages/client/ecrireRecette.php
+++ pages/client/ecrireRecette.php
@@ -6,6 +6,13 @@
     <!-- Dépendances -->
     <?=Client::getDependencies()?>
     <title><?=getWebsiteSetting("websiteName")?> | Écrire une recette</title>
+
+    <!-- Summernote -->
+	<link rel="stylesheet" href="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/summernote/dist/summernote-bs4.css">
+	<script type="text/javascript" src="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/summernote/dist/summernote-bs4.js"></script>
+	<link href="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/tam-emoji/css/emoji.css" rel="stylesheet">
+	<script src="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/tam-emoji/js/config.js"></script>
+  	<script src="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/tam-emoji/js/tam-emoji.min.js"></script>
     
     <!-- Embed -->
     <meta content="<?=getWebsiteSetting("websiteName")?>" property="og:title" />
@@ -22,13 +29,51 @@
     <?=Client::getNavbar()?>
 
     <!-- Recettes -->
-    <div class="container">
+    <div class="container mt-5">
         <h4>Écrire une recette</h4>
+
+        <div class="row">
+            <div class="col-sm-6 col-editor-content">
+                <input type="text" class="form-control my-2" id="recetteTitle" placeholder="Une superbe recette">
+                <textarea required id="summernote" name="recetteContent"></textarea>
+            </div>
+            <div class="col-6 col-editor-sidebar">
+                <div class="card vbcard">
+                    <h6 class="card-header">Publier</h6>
+                    <div class="card-body">
+                        <button type="button" onclick="autoSave(0)" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Brouillon</button>
+                        <button type="button" onclick="preview()" class="btn btn-secondary btn-sm"><i class="fas fa-eye"></i> Prévisualiser</button>
+                        <p class="card-text mt-2"><strong>Choisir une image d'entête</strong></p>
+                        <a href="#" onclick="openGallery()" class="text-dark"><div id="editor-headerPic" class="editor-headerPic border rounded"><i class="fas fa-image"></i>Ajouter</div></a>
+                        <a href="#" onclick="publish()" class="btn btn-primary" style="margin-top: .75rem;">Publier</a>
+                        
+                    </div>
+                </div>
+
+                <div class="card vbcard mt-3">
+                    <h6 class="card-header"><?=$translation["description"]?></h6>
+                    <div class="card-body">
+                        <p class="card-text"><strong><?=$translation["blog_message_1"]?></strong></p>
+                        <textarea id="recetteDescription" class="form-control"></textarea>
+                    </div>
+                </div>
+
+                <div class="card vbcard mt-3">
+                    <h6 class="card-header"><?=$translation["blog_category"]?></h6>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <select multiple class="form-control" id="recetteCategory">
+                                <option value="0">Aucune</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <?=Client::getFooter()?>
 
-    <script src="<?=getWebsiteSetting("websiteUrl")?>pages/assets/vendors/flickity/js/flickity.pkgd.min.js"></script>
     <script type="text/javascript">
         
     </script>
