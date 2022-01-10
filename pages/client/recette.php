6517e89cbc46a2d3e67d1f749d3892116a8f8e00
suprasanity <yann.jeanmaire@gmail.com>
2022-01-10 11:19:54 +0100

    s

--- pages/client/recette.php
+++ pages/client/recette.php
@@ -18,7 +18,7 @@
         if(isset($_GET['recetteId']) && !empty($_GET['recetteId']))
         $recetteId=$_GET['recetteId'];
         
-        prin_r($recette=getRecettes(array("categoryId" => $recetteId)));
+        print_r($recette=getRecettes(array("categoryId" => $recetteId)));
         ?>
 <div class="receipe-content-area">
             <div class="container">
