c000231b83531ab77ad1f2777a22e0018afe9917
suprasanity <yann.jeanmaire@gmail.com>
2022-01-10 11:16:33 +0100

    mabite

--- pages/client/recette.php
+++ pages/client/recette.php
@@ -40,7 +40,7 @@
                         <div class="receipe-ratings text-right my-5">
                             <div class="ratings">
                                 <?php
-                                for ($i = 1; $i <= $recette[0]["difficulte"]; $i++){
+                                for ($i = 0; $i < $recette[0]["difficulte"]; $i++){
                                  echo '<i class="fa fa-star" aria-hidden="true"></i>';
                                 }
                                  echo ' </div>';
