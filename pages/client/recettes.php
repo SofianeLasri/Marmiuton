4dfd39b37d8e08c7274920885ae3985f245a6aa0
SofianeLasri <alasri250@gmail.com>
2022-01-12 11:12:44 +0100

    better now

--- pages/client/recettes.php
+++ pages/client/recettes.php
@@ -27,7 +27,7 @@
                     <div class="form-group">
                         <label>Catégorie</label>
                         <select class="form-control" name="categoryId">
-                            <option>Non spécifié</option>
+                            <option value="">Non spécifié</option>
                             <?php
                                 $categories = Recette::getCategories();
                                 foreach($categories as $category) {
