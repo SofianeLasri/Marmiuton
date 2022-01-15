08d95a72ca3af4a95952c5ccbc2987bb93dd9a47
SofianeLasri <alasri250@gmail.com>
2022-01-15 09:47:54 +0100

    hide comment section if not connected

--- pages/client/recette.php
+++ pages/client/recette.php
@@ -104,39 +104,34 @@ if(isset($_GET['recetteId']) && !empty($_GET['recetteId'])){
                     </div>
                 </div>
 
-                <div class="row">
+                <?php
+                if(isset($_SESSION["userId"])){
+                    echo('<div class="row">
                     <div class="col-12">
                         <div class="section-heading text-left">
                             <h3>Envoyer un commentaire</h3>
                         </div>
                     </div>
                 </div>
-
                 <div class="row">
                     <div class="col-12">
                         <div class="contact-form-area">
                             <form action="#" method="post">
                                 <div class="row">
-                                    <div class="col-12 col-lg-6">
-                                        <input type="text" class="form-control" id="name" placeholder="Name">
-                                    </div>
-                                    <div class="col-12 col-lg-6">
-                                        <input type="email" class="form-control" id="email" placeholder="E-mail">
-                                    </div>
-                                    <div class="col-12">
-                                        <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    </div>
                                     <div class="col-12">
                                         <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="Message"></textarea>
                                     </div>
                                     <div class="col-12">
-                                        <button class="btn btn-orange mt-3" type="submit">Post Comments</button>
+                                        <button class="btn btn-orange mt-3" type="submit">Envoyer</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
+                </div>');
+                }
+                ?>
+                
             </div>
         </div>
     </div>
