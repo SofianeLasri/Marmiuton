56e60597a41e62d7b696ecb925b8e956f00841c8
SofianeLasri <alasri250@gmail.com>
2025-04-01 16:54:58 +0200

    configure environment variables and add Docker setup

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM php:7.4-apache
+
+RUN a2enmod rewrite
+RUN docker-php-ext-install pdo pdo_mysql
+
+COPY . /var/www/html/
+
+RUN chown -R www-data:www-data /var/www/html \
+    && chmod -R 755 /var/www/html
+
+WORKDIR /var/www/html
+
+EXPOSE 80
--- core/conf/ConfigurationGenerale.php
+++ core/conf/ConfigurationGenerale.php
@@ -1,9 +1,9 @@
 <?php
 // attributs de la classe Connexion paramètres de connexion à la base
-$bddHost = 'localhost';
-$bddName = 'marmiuton';
-$bddUsername = 'marmiuton';
-$bddUserPassword = 's6rqTiA0hNKmcgy7';
+$bddHost = $_ENV['DB_HOST'] ?? 'localhost';
+$bddName = $_ENV['DB_NAME'] ?? 'marmiuton';
+$bddUsername = $_ENV['DB_USER'] ?? 'marmiuton_user';
+$bddUserPassword = $_ENV['DB_PASSWORD'] ?? 'password';
 
 // Cette variable défini le mode de reconnaissance des url
 // Sur le serveur de l'IUT, on ne peut pas utiliser les alias de l'url pour détecter les pages, vu que nous ne sommes pas sur la racine du domaine
